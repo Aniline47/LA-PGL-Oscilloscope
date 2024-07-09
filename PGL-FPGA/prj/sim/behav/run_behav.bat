b691d0b1c46e6dd9acd63505cb076b816730d3a4
Aniline47 <117579058+Aniline47@users.noreply.github.com>
2024-07-10 01:54:51 +0800

    Add files via upload

---
+++ PGL-FPGA/prj/sim/behav/run_behav.bat
@@ -0,0 +1,10 @@
+@echo off
+set bin_path=D:/modeltech64_10.7/win64
+cd D:/pango/MyProject/oscillosope/prj/sim/behav
+call "%bin_path%/modelsim"   -do "do {run_behav_compile.tcl};do {run_behav_simulate.tcl}" -l run_behav_simulate.log
+if "%errorlevel%"=="1" goto END
+if "%errorlevel%"=="0" goto SUCCESS
+:END
+exit 1
+:SUCCESS
+exit 0
